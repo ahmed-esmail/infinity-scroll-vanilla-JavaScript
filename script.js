1962f479b55b065b83b4934e5fea6309310d040e
ahmed-esmail <51207001+ahmed-esmail@users.noreply.github.com>
2021-04-02 16:14:53 +0200

    Scroll implemented

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let totalImagesLoaded = 0;
+let totalImages = 0;
+let photoArray = [];
+
 const apiKey = "IkYiYKiyhS6vrA_2UqcKyfu4pwOMj4Qv0HZArxMUv8Y";
-const count = 10;
+const count = 6;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&&count=${count}
 `;
 
@@ -17,9 +22,10 @@ async function getPhotos() {
 }
 
 function displayImage(photosArray) {
+  totalImagesLoaded = 0;
+  totalImages = photosArray.length;
   photosArray.forEach((image) => {
     // ancher element for photos
-    console.log(image);
     const anchorElement = document.createElement("a");
     setAttribute(anchorElement, {
       href: image.links.html,
@@ -32,16 +38,39 @@ function displayImage(photosArray) {
       alt: image.alt_description,
       title: image.alt_description,
     });
+    // check if image is loaded
+    imageElement.addEventListener("load", isImageLoaded);
     // put image inside anchor tag and anchor tage inside imageContainer
-
     anchorElement.appendChild(imageElement);
     imageContainer.appendChild(anchorElement);
   });
 }
 
+// check to se if scroll near botoms
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+// check if image is loaded
+
 function setAttribute(element, attribute) {
   for (const key in attribute) {
     element.setAttribute(key, attribute[key]);
+  }
+}
+
+function isImageLoaded() {
+  totalImagesLoaded++;
+  if (totalImagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
   }
 }
 
